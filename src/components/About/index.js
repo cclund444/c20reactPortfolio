b0fd55d12591d70f609c420090dd7d58eb094ae2
cclund444 <87826114+cclund444@users.noreply.github.com>
2022-01-06 19:59:40 -0700

    update about me display

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,26 +2,35 @@ import React from 'react';
 // import './style.css';
 import Container from 'react-bootstrap/Container';
 import Figure from 'react-bootstrap/Figure';
-
+import { Col, Image, Row } from 'react-bootstrap';
+import selfie from '../../assets/1.jpg';
 
 function About() {
     return (
       <Container>
           <h1>About Me</h1>
+        {/* <Image
+          src={selfie} thumbnail></Image> */}
+          <Row>
+            <Col md="3">
           <Figure>
             <Figure.Image
               width={171}
-              heigh={180}
+              height={180}
               alt="Caleb Lund"
-              img src="./assets/1.jpg" 
+              src={selfie}
             />
           </Figure>
-          <p>
+          </Col>
+          <Col>
+          <p class="fs-3">
           I hope this finds whoever is reading it doing well. This is a portfolio of my codeing. I am learning and enjoying it. Ask any questions. 
           </p>
           <p>
           I have obtained my Bachelors Degree from The University of Arizona in; General Studies with an imphasis on Science, Technology, Health and Society. I am currently towards the end of a BootCamp through UofA for codeing and web developement. 
           </p>
+          </Col>
+          </Row>
       </Container>
       
     );
